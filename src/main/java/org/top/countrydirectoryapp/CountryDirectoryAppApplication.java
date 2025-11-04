bcf9aab026967ff428c104f56752506b0986817f
AndreyCorpse28090 <280990corpse@gmail.com>
2025-11-04 18:48:00 +0300

    Initial commit

---
+++ src/main/java/org/top/countrydirectoryapp/CountryDirectoryAppApplication.java
@@ -0,0 +1,13 @@
+package org.top.countrydirectoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDirectoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryDirectoryAppApplication.class, args);
+	}
+
+}
